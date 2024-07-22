262ea669f809209abc29390fbb01fb83bc421177
Carlos Molina <carlosm12.inves@gmail.com>
2024-07-22 11:29:23 +0000

    generador JS creado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,41 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = function() {};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const valores = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+const simbolos = ["&#9829;", "&#9830;", "&#9827;", "&#9824;"]; // Corazón, Diamante, Trébol, Pica
+
+function obtenerCartaAleatoria() {
+  const valorAleatorio = valores[Math.floor(Math.random() * valores.length)];
+  const simboloAleatorio =
+    simbolos[Math.floor(Math.random() * simbolos.length)];
+
+  return `
+        <div class="carta-cabecera">${valorAleatorio}</div>
+        <div class="simbolo">${simboloAleatorio}</div>
+        <div class="carta-pie">${valorAleatorio}</div>
+    `;
+}
+
+document.getElementById("boton").addEventListener("click", function() {
+  document.getElementById("carta").innerHTML = obtenerCartaAleatoria();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("carta").innerHTML = obtenerCartaAleatoria();
+});
